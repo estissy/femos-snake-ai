bc9c09692e2866fcfa68bc7a62f749eea8863340
Jakub Wietecha <estissy@gmail.com>
2019-11-01 14:56:38 +0100

    Added support for command line arguments

--- unssg-evolution.py
+++ unssg-evolution.py
@@ -1,3 +1,4 @@
+import argparse
 from statistics import mean
 
 from femos.core import get_number_of_nn_weights, get_evolved_population
@@ -7,44 +8,59 @@ from femos.selections import get_two_size_tournament_parent_selection, get_age_b
 
 from engine.game import Game
 
-game_board_width = 36
-game_board_height = 18
-seed = 777
-initial_snake_length = 5
+# Parsing command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--game_board_width", help="Width of the snake game board.", type=int, default=36)
+parser.add_argument("--game_board_height", help="Height of the snake game board.", type=int, default=18)
+parser.add_argument("--seed", help="Selected integer value for experiments recurrence.", type=int, default=777)
+parser.add_argument("--initial_snake_length", help="Initial snake length in new game.", type=int, default=5)
+parser.add_argument("--hidden_layer_nodes", help="List of nodes in hidden layers", type=list, default=[64])
+parser.add_argument("--weight_lower_threshold", help="Lower threshold value of ann weights", type=float, default=-1.0)
+parser.add_argument("--weight_upper_threshold", help="Upper threshold value of ann weights", type=float, default=1.0)
+parser.add_argument("--mutation_step_size_lower_threshold",
+                    help="Lower threshold value for genotype mutation step size",
+                    type=float, default=-0.2)
+parser.add_argument("--mutation_step_size_upper_threshold",
+                    help="Upper threshold value for genotype mutation step size",
+                    type=float, default=0.2)
+parser.add_argument("--population_size", help="Number of individuals in population", type=int, default=20)
+parser.add_argument("--epochs", help="Number of epochs", type=int, default=1000)
+parser.add_argument("--tau1", help="Mutation operator parameter - tau1", type=float, default=0.001)
+parser.add_argument("--tau2", help="Mutation operator parameter - tau2", type=float, default=0.01)
+parser.add_argument("--snack_eaten_points", help="Number of points assigned when snake eat snack", type=float,
+                    default=1.0)
+parser.add_argument("--moving_toward_snack_points", help="Number of points assigned when snake is moving toward snack",
+                    type=float, default=0.1)
+parser.add_argument("--moving_away_snack_points", help="Number of points assigned when snake is moving away of snack",
+                    type=float, default=-0.2)
+parser.add_argument("--max_points_threshold", help="Number of points to end game even if snake still plays.",
+                    type=float,
+                    default=10.0)
 
-input_nodes = game_board_width * game_board_height + 4
-hidden_layer_nodes = [64]
+args = parser.parse_args()
+
+input_nodes = args.game_board_width * args.game_board_height + 4
 output_nodes = 3
-number_of_nn_weights = get_number_of_nn_weights(input_nodes, hidden_layer_nodes, output_nodes)
-weight_lower_threshold = -1
-weight_upper_threshold = 1
-mutation_step_size_lower_threshold = -0.3
-mutation_step_size_upper_threshold = 0.3
-population_size = 20
-epochs = 1000
-tau1 = 0.001
-tau2 = 0.01
-snack_eaten_points = 1
-moving_toward_snack_points = 0.1
-moving_away_snack_points = -0.2
+number_of_nn_weights = get_number_of_nn_weights(input_nodes, args.hidden_layer_nodes, output_nodes)
 
-genotypes = UncorrelatedNStepSizeGenotype.get_random_genotypes(population_size, number_of_nn_weights,
-                                                               weight_lower_threshold, weight_upper_threshold,
-                                                               mutation_step_size_lower_threshold,
-                                                               mutation_step_size_upper_threshold)
+genotypes = UncorrelatedNStepSizeGenotype.get_random_genotypes(args.population_size, number_of_nn_weights,
+                                                               args.weight_lower_threshold, args.weight_upper_threshold,
+                                                               args.mutation_step_size_lower_threshold,
+                                                               args.mutation_step_size_upper_threshold)
 
 
 def phenotype_strategy(genotype):
-    return Phenotype(genotype.weights, input_nodes, hidden_layer_nodes, output_nodes)
+    return Phenotype(genotype.weights, input_nodes, args.hidden_layer_nodes, output_nodes)
 
 
 def evaluation_strategy(phenotypes):
     phenotype_values = []
 
     for selected_phenotype in phenotypes:
-        initial_game = Game(game_board_width, game_board_height, selected_phenotype, seed,
-                            Game.get_full_game_representation_strategy, initial_snake_length, snack_eaten_points,
-                            moving_toward_snack_points, moving_away_snack_points)
+        initial_game = Game(args.game_board_width, args.game_board_height, selected_phenotype, args.seed,
+                            Game.get_full_game_representation_strategy, args.initial_snake_length,
+                            args.snack_eaten_points,
+                            args.moving_toward_snack_points, args.moving_away_snack_points, args.max_points_threshold)
         solved_game = Game.get_solved_game(initial_game)
         phenotype_values.append(solved_game.score)
 
@@ -53,11 +69,11 @@ def evaluation_strategy(phenotypes):
 
 
 def parent_selection_strategy(phenotypes_values):
-    return get_two_size_tournament_parent_selection(phenotypes_values, population_size)
+    return get_two_size_tournament_parent_selection(phenotypes_values, args.population_size)
 
 
 def mutation_strategy(genotype):
-    return UncorrelatedNStepSizeGenotype.get_mutated_genotype(genotype, tau1, tau2)
+    return UncorrelatedNStepSizeGenotype.get_mutated_genotype(genotype, args.tau1, args.tau2)
 
 
 def offspring_selection_strategy(parents, updated_parents):
@@ -66,4 +82,4 @@ def offspring_selection_strategy(parents, updated_parents):
 
 evolved_population = get_evolved_population(genotypes, phenotype_strategy, evaluation_strategy,
                                             parent_selection_strategy, mutation_strategy, offspring_selection_strategy,
-                                            epochs)
+                                            args.epochs)
